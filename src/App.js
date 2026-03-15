cc02242873811bbcba923ff7983cda51f4d74090
SazidWork <sazid.jobwork@gmail.com>
2026-03-15 17:07:12 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>My React Ecommerce Store</h1>
+      <p>Welcome to my online store</p>
+    </div>
+  );
+}
+
+export default App;
